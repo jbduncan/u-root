09ebe54d70a468ce96ac1b1a2dd797ec9799692e
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2025-06-18 16:40:21 +0100

    WIP: start replacing Kahn's algorithm with Knuth's algorithm

--- cmds/extra/tsort/tsort.go
+++ cmds/extra/tsort/tsort.go
@@ -153,45 +153,73 @@ func parseInto(buf string, g *graph) error {
 	return nil
 }
 
+type nodeMetadata struct {
+	// Equivalent to COUNT and QLINK in Knuth's algorithm
+	inDegree int
+	// Equivalent to TOP in Knuth's algorithm
+	successors []string
+}
+
 func topologicalOrdering(
-	g *graph,
+	input string,
 	f func(node string),
 	cycles func(cycle []string),
-) {
+) error {
+	fields := strings.Fields(input)
+
 	for {
-		// Kahn's algorithm
+		// Topological Sort algorithm from "The Art of Programming, Volume 1" Third
+		// Edition, 2.2.3 by Donald E. Knuth.
+		var i int
+		var odd bool
+		next := func() (string, bool) {
+			if i == len(fields) {
+				return "", false
+			}
+			odd = !odd
+			result := fields[i]
+			i++
+			return result, true
+		}
+
 		var result []string
-		roots := rootsOf(g)
-		nonRoots := nonRootsOf(g)
-		for !roots.isEmpty() {
-			next := roots.dequeue()
-			result = append(result, next)
-			for succ := range g.successors(next) {
-				nonRoots.removeOne(succ)
-				if !nonRoots.has(succ) {
-					roots.enqueue(succ)
-				}
-			}
-		}
-		if nonRoots.isEmpty() {
-			// No cycles left
-			for _, value := range result {
-				f(value)
-			}
-			break
-		}
+		nodes := make(map[string]nodeMetadata)
+		// TODO: print lone nodes at the end
+		var loneNodes []string
 
-		// Break a cycle and try Kahn's algorithm again
-		nonRoots.forEachUnique(func(next string) bool {
-			cycle := cycleStartingAt(g, next)
-			if len(cycle) == 0 {
-				return true
+		for {
+			a, ok := next()
+			if !ok {
+				break
 			}
 
-			g.removeEdge(cycle[len(cycle)-1], cycle[0])
-			cycles(cycle)
-			return false
-		})
+			b, ok := next()
+			if !ok {
+				break
+			}
+
+			if a == b {
+				loneNodes = append(loneNodes, a)
+				continue
+			}
+
+			nodes[a] = nodeMetadata{
+				inDegree:   nodes[a].inDegree,
+				successors: append(nodes[a].successors, b),
+			}
+			nodes[b] = nodeMetadata{
+				inDegree:   nodes[b].inDegree + 1,
+				successors: nodes[b].successors,
+			}
+		}
+
+		if odd {
+			return errOddDataCount
+		}
+
+		// TODO:
+		//  1. Implement Donald Knuth's Topological Sort algorithm ("The Art of Programming, Volume 1" 3rd ed., 2.2.3)
+		//  2. If any cycles are found, break a cycle and try the algorithm again
 	}
 }
 
